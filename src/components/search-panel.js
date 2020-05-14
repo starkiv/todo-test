de4e0d240024e623690b7f5a2b4c817c3924836b
starkiv <yuriistarkiv@gmail.com>
2020-05-14 16:35:59 +0100

    Added JSX and directory 'components'

---
+++ src/components/search-panel.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const SearchPanel = () => {
+    const searchText = 'search';
+    return <input placeholder={ searchText } />
+}
+
+export default SearchPanel;
---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import AppHeader from './components/app-header';
+import ToDoList from './components/todo-list';
+import SearchPanel from './components/search-panel';
+
+const App = () => {
+    return (<div>
+        <AppHeader />
+        <SearchPanel />
+        <ToDoList />
+    </div>)
+}
+
+ReactDOM.render(<App />,
+    document.getElementById('root'));
